9a686855016e3eccc6919f7d5712e8247dbc84df
adnanfuad697 <adnanfuad697@gmail.com>
2026-08-05 14:07:56 +0600

    Update faq-section.tsx

--- components/faq-section.tsx
+++ components/faq-section.tsx
@@ -45,7 +45,7 @@ export default function FaqSection() {
   }, [])
 
   return (
-    <section id="faq" className="py-20 bg-white">
+    <section id="faq" className="py-12 sm:py-16 lg:py-20 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-12">
           <div className="lg:col-span-1">
